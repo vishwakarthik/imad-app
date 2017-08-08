75f5718bc5dd323fed363bafe31b182bf7ab7a0e
vishwakarthik <30723112+vishwakarthik@users.noreply.github.com>
2017-08-08 19:35:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,8 @@
-console.log('Loaded!');
-
-// Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-madi.onclick = function () {
-    var interval = setInterval(moveRight, 100);
-    madi.style.marginLeft = '100px';
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+    
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString(); 
 };
